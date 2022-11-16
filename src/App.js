8966bbad0b5b398d670381bddc130db12977ba50
emad-hash <emadhashash76@gmail.com>
2022-11-17 01:01:07 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { Routes , Route ,NavLink } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Login" element={<Login/>}/>
+        <Route path="/Register" element={<Register/>}/>
+
+
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function register() {
+  return (
+    <div>
+    <div className="-400 h-screen w-full">
+      <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
+        <div className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 bg-white sm:mx-0" style={{height: '500px'}}>
+          <div className="flex flex-col w-full md:w-1/2 p-4">
+            <div className="flex flex-col flex-1 justify-center mb-8">
+              <h1 className="text-4xl text-center font-thin">Register</h1>
+              <div className="w-full mt-4">
+                <form className="form-horizontal w-3/4 mx-auto" method="POST" action="#">
+                <div className="flex flex-col mt-4">
+                    <input id="fname" type="text" className="flex-grow h-8 px-2 border rounded border-grey-400" name="fname"  placeholder="First Name" />
+                  </div>
+                  <div className="flex flex-col mt-4">
+                    <input id="lname" type="text" className="flex-grow h-8 px-2 border rounded border-grey-400" name="lname"  placeholder="Last Name" />
+                  </div>
+                  <div className="flex flex-col mt-4">
+                    <input id="email" type="text" className="flex-grow h-8 px-2 border rounded border-grey-400" name="email"  placeholder="Email" />
+                  </div>
+                  <div className="flex flex-col mt-4">
+                    <input id="password" type="password" className="flex-grow h-8 px-2 rounded border border-grey-400" name="password" required placeholder="Password" />
+                  </div>
+                  <div className="flex items-center mt-4">
+                    <input type="checkbox" name="remember" id="remember" className="mr-2" /> <label htmlFor="remember" className="text-sm text-grey-dark">Remember Me</label>
+                  </div>
+                  <div className="flex flex-col mt-8">
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-semibold py-2 px-4 rounded">
+                    Register
+                    </button>
+                  </div>
+                </form>
+                <div className="text-center mt-4">
+                  <Link className="no-underline hover:underline text-blue-dark text-2xl" to="/Login">
+                  Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hidden md:block md:w-1/2 rounded-r-lg" style={{background: 'url("https://images.unsplash.com/photo-1515965885361-f1e0095517ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80")', backgroundSize: 'cover', backgroundPosition: 'center center'}} />
+        </div>
+      </div>
+    </div></div>
+  )
+}
+
+export default register
