03300b64b9094ac5209eb3fa758ff60db7680473
emad-hash <emadhashash76@gmail.com>
2022-11-18 15:09:45 +0300

    css

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import { Routes , Route ,NavLink } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
-import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './pages/home/Home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/movieDetail/Movie';
 
 
 function App() {
   return (
     <div>
       <Nav/>
+      
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
         <Route path="/Contact" element={<Contact/>}/>
         <Route path="/Login" element={<Login/>}/>
         <Route path="/Register" element={<Register/>}/>
-        {/* <Route path="Slider/:id" element={<Slider />}></Route>
-                <Route path="movies/:type" element={<MovieList />}></Route> */}
+        <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
       </Routes>
       <Footer/>
     </div>
