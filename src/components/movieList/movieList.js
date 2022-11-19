dd9ac39bea7484b7544d19762c91b714d90b39b9
emad-hash <emadhashash76@gmail.com>
2022-11-19 19:45:02 +0300

    css

--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react"
 import "./movieList.css"
 import { useParams } from "react-router-dom"
 import Cards from "../card/card"
+import Search from "../../Search"
 
 const MovieList = () => {
     
@@ -17,24 +18,17 @@ const MovieList = () => {
     }, [type])
 
     const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
         .then(data => setMovieList(data.results))
     }
 
+
+
     return (
         <div className="movie__list">
-            <form className='mt-8'>   
-    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-    <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-        </div>
-        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Filem" required/>
-        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
-            <h2 className="list__title">{(type ? type : "").toUpperCase()}</h2>
+           {/* <Search    /> */}
+            {/* <h2 className="list__title">{(type ? type : "").toUpperCase()}</h2> */}
             <div className="list__cards">
                 {
                     movieList.map(movie => (
