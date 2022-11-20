9831ae19857d49d8a881243b507c399a53fef12a
emad-hash <emadhashash76@gmail.com>
2022-11-20 17:43:18 +0300

    search

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -24,6 +24,7 @@ const Cards = ({movie}) => {
         </div>
         :
         <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
+       
             <div className="cards">
                 <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
                 <div className="cards__overlay">
--- src/components/movieList/movieList.js
+++ src/components/movieList/movieList.js
@@ -2,36 +2,48 @@ import React, {useEffect, useState} from "react"
 import "./movieList.css"
 // import { useParams } from "react-router-dom"
 import Cards from "../card/card"
-// import Search from "../../Search"
+import Search from "../../Search"
 
 const MovieList = () => {
     
     const [movieList, setMovieList] = useState([])
+    const [newList , setNewList] = useState([])
+    const [search , setSearch] =useState("")
     // const {type} = useParams()
 
     useEffect(() => {
         getData()
     }, [])
 
-    useEffect(() => {
-        getData()
-    }, [type])
+    // useEffect(() => {
+    //     getData()
+    // }, [type])
 
     const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
-        .then(data => setMovieList(data.results))
+        .then(data => {
+            // console.log(data.results);
+            setMovieList(data.results)
+            setNewList(data.results)
+        })
     }
 
-
+    useEffect(()=>{
+        let  arr =[];
+        arr= movieList?.filter(e=>{
+            return e.title.toLowerCase().includes(search.toLowerCase());
+        })
+        setNewList(arr);
+    },[search])
 
     return (
         <div className="movie__list">
-           {/* <Search    /> */}
+           <Search setSearch={setSearch}/>
             {/* <h2 className="list__title">{(type ? type : "").toUpperCase()}</h2> */}
             <div className="list__cards">
                 {
-                    movieList.map(movie => (
+                    newList?.map(movie => (
                         <Cards movie={movie} />
                     ))
                 }
