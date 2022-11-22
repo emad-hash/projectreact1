2fe4b2030d22fc7310b929d8b76bd502ce92ccca
emad-hash <emadhashash76@gmail.com>
2022-11-22 16:48:23 +0300

    add fav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -46,7 +46,7 @@ const Nav = () => {
                 
               </li>
               <li>
-              <NavLink to='/Fav' class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">fav</NavLink>
+              <NavLink to='/Fav' class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Favorite</NavLink>
                 
               </li>
             
